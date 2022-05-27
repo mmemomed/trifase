3a54c280596ef90b311b19f75a9b5a40e1e08c3b
unknown <mgmed@outlook.com>
2022-05-27 16:29:54 -0500

    Se hará el código primero con un monitoreo monofásico

---
+++ monofase.py
@@ -0,0 +1,43 @@
+#Obtención de datos de un motor trifásico
+
+import serial, serial.tools.list_ports #Library to identify the ports
+
+
+# ESTABLECER EL PUERTO AL QUE ESTÁ CONECTADO EL ARDUINO
+
+def obtpuerto(): #Obtiene todos los puertos disponibles
+    puertos = serial.tools.list_ports.comports() #Variable with the available ports
+    return puertos
+
+def encArduino(obtpuerto): #Determinara a que puerto esta conectado el arduino
+    commPort = 'None'
+    conexiones = len(obtpuerto)
+
+    for i in range(0,conexiones):
+        puerto = obtpuerto[i]
+        puertostring = str(puerto)
+
+        if 'Arduino' in puertostring:
+            separaPuerto = puertostring.split(' ')
+            commPort = (separaPuerto[0])
+    
+    return commPort
+
+puertosEnc = obtpuerto()
+conPuerto = encArduino(puertosEnc)
+
+if conPuerto != 'None': #Una vez encontrado el arduino nos imprimira en que puerto esta conectado
+    ser = serial.Serial(conPuerto, baudrate = 115200, timeout = 0.2)
+
+    print('Conectado al puerto: ' + conPuerto)
+else:
+    print('Error de Conexión')
+
+#RECOGE LOS DATOS Y LOS ALMACENA EN UNa lISTA
+
+size = 200
+voltaje = []
+corriente = []
+
+for i in range(size):
+    readline
