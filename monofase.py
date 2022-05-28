c4f5d43424d8f4fdcc85b0f13356c025ff0b8a9e
unknown <mgmed@outlook.com>
2022-05-28 16:08:10 -0500

    5mentarios

--- monofase.py
+++ monofase.py
@@ -1,7 +1,7 @@
 #Obtención de datos de un motor trifásico
 
 import serial, serial.tools.list_ports #Library to identify the ports
-
+import time
 
 # ESTABLECER EL PUERTO AL QUE ESTÁ CONECTADO EL ARDUINO
 
@@ -27,28 +27,48 @@ puertosEnc = obtpuerto()
 conPuerto = encArduino(puertosEnc)
 
 if conPuerto != 'None': #Una vez encontrado el arduino nos imprimira en que puerto esta conectado
-    ser = serial.Serial(conPuerto, baudrate = 115200, timeout = 0.2)
+    ser = serial.Serial(conPuerto, baudrate = 9600, timeout = 0)
 
     print('Conectado al puerto: ' + conPuerto)
 else:
     print('Error de Conexión')
 
 #RECOGE LOS DATOS Y LOS ALMACENA EN UNA lISTA
+temperatura = [] #lista para almacenar los datos de voltaje
+humedad = [] #Lista para almacenar los datos de corriente
 
-size = 200
-voltaje = [] #lista para almacenar los datos de voltaje
-corriente = [] #Lista para almacenar los datos de corriente
+while True:    
+    puertosEnc = obtpuerto()
+    conPuerto = encArduino(puertosEnc)
+    if conPuerto == 'None':     #ROMPE EL CICLO SI SE DESCONECTA EL ARDUINO
+        break
+    linea = ser.readline()
+    string = linea.decode()
+    if string != '':
+        sep = string.split(',')
+        pvar = float(sep[1])
+        svar = float(sep[2])
+        humedad.append(pvar)
+        temperatura.append(svar)
+        #print(pvar)
+        #print(svar)
+
+    time.sleep(1)
+
+print('Terminado')
+print(temperatura)
+print(humedad)
 
 
 
+'''
 for i in range(size):
     linea = ser.readline() #Lectura de líneas de datos
     string = linea.decode() #Transformar los datos a string
     sep = string.split(",") 
     voltaje.append(sep[1]) #Obtener el voltaje de esta línea
     corriente.append(sep[2]) #Obtener la corriente de esta línea
+'''
 
-#Hacer un if para saber si esta conectado que hacer
-#Que se rompa cuando se desconecte el arduino
 
 
